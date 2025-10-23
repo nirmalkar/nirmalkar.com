2ea30e7184a4fc7a54e9d73bd7e1e2a46deb5f9b
nirmalkar <hemantnirmalkar17@gmail.com>
2025-10-24 00:11:03 +0530

    work cards, technologies icons and layout changes

--- src/components/TechIcon/index.tsx
+++ src/components/TechIcon/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../../context/themeProvider";
 import { SiMaterialUi } from "@react-icons/all-files/si/SiMaterialUi";
 import { SiJavascript } from "@react-icons/all-files/si/SiJavascript";
 import { FaReact } from "@react-icons/all-files/fa/FaReact";
@@ -23,50 +24,64 @@ import { SiTypescript } from "@react-icons/all-files/si/SiTypescript";
 type TechIconsPropsType = {
   name: string;
   size?: number;
+  color?: string;
 };
 
-function TechIcons({ name, size = 30 }: TechIconsPropsType) {
+const defaultIconProps = (color: string) => ({
+  style: { color },
+  'aria-hidden': true
+});
+
+function TechIcons({ name, size = 24, color }: TechIconsPropsType) {
+  const { theme } = useContext(ThemeContext);
+  const iconColor = color || theme?.colors?.oppositePrimary || '#ffffff';
+  const iconProps = {
+    size,
+    title: name,
+    ...defaultIconProps(iconColor)
+  };
+
   switch (name.toLowerCase()) {
     case "javascript":
-      return <SiJavascript title={name} size={size} />;
+      return <SiJavascript {...iconProps} />;
     case "typescript":
-      return <SiTypescript title={name} size={size} />;
+      return <SiTypescript {...iconProps} />;
     case "reactjs":
-      return <FaReact title={name} size={size} />;
+      return <FaReact {...iconProps} />;
     case "vue":
-      return <FaVuejs title={name} size={size} />;
+      return <FaVuejs {...iconProps} />;
     case "jest":
-      return <SiJest title={name} size={size} />;
+      return <SiJest {...iconProps} />;
     case "python":
-      return <FaPython title={name} size={size} />;
+      return <FaPython {...iconProps} />;
     case "nodejs":
-      return <FaNodeJs title={name} size={size} />;
+      return <FaNodeJs {...iconProps} />;
     case "postgresql":
-      return <DiPostgresql title={name} size={size} />;
+      return <DiPostgresql {...iconProps} />;
     case "docker":
-      return <FaDocker title={name} size={size} />;
+      return <FaDocker {...iconProps} />;
     case "git":
-      return <FaGit title={name} size={size} />;
+      return <FaGit {...iconProps} />;
     case "aws":
-      return <FaAws title={name} size={size} />;
+      return <FaAws {...iconProps} />;
     case "graphql":
-      return <SiGraphql title={name} size={size} />;
+      return <SiGraphql {...iconProps} />;
     case "adonisjs":
-      return <SiAdonisjs title={name} size={size} />;
+      return <SiAdonisjs {...iconProps} />;
     case "antdesign":
-      return <AiOutlineAntDesign title={name} size={size} />;
+      return <AiOutlineAntDesign {...iconProps} />;
     case "bootstrap":
-      return <FaBootstrap title={name} size={size} />;
+      return <FaBootstrap {...iconProps} />;
     case "sass":
-      return <FaSass title={name} size={size} />;
+      return <FaSass {...iconProps} />;
     case "html":
-      return <FaHtml5 title={name} size={size} />;
+      return <FaHtml5 {...iconProps} />;
     case "css":
-      return <FaCss3 title={name} size={size} />;
+      return <FaCss3 {...iconProps} />;
     case "mui":
-      return <SiMaterialUi title={name} size={size} />;
+      return <SiMaterialUi {...iconProps} />;
     case "tailwind":
-      return <SiTailwindcss title={name} size={size} />;
+      return <SiTailwindcss {...iconProps} />;
     default:
       return null;
   }
