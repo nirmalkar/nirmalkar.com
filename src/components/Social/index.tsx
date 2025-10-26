2a141c4ba7867b6717bb095e6b9f5d1f37e4b0dc
nirmalkar <hemantnirmalkar17@gmail.com>
2025-10-26 23:54:50 +0530

    make social icon bg color white on light theme

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -6,10 +6,15 @@ import { ThemeContext } from '../../context/themeProvider';
 
 const Social: FC = () => {
   const { theme } = React.useContext(ThemeContext);
-  const { secondary, oppositeSecondary } = theme?.colors || {
+  const { secondary, oppositeSecondary, primary } = theme?.colors || {
     secondary: '#f0f0f0',
-    oppositeSecondary: '#333333'
+    oppositeSecondary: '#333333',
+    primary: '#ffffff',
   };
+
+  const backgroundColor =
+    theme?.colors?.primary === '#ffffff' ? primary : secondary;
+
   return (
     <div className="social-container">
       {social.map((social) => (
@@ -19,7 +24,7 @@ const Social: FC = () => {
           target="_blank"
           rel="noopener noreferrer"
           className="social"
-          style={{ backgroundColor: secondary }}
+          style={{ backgroundColor }}
         >
           <Icon fill={oppositeSecondary} name={social.name} />
         </a>
