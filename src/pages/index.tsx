4b4a04767a26c77984e2c2eee35ed32b8fd3dcc5
nirmalkar <hemantnirmalkar17@gmail.com>
2026-01-23 20:27:51 +0530

    refactor: types, seo and optimization landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,41 @@
 import type { PageProps } from 'gatsby';
 import { graphql } from 'gatsby';
-import { get } from 'lodash';
-import * as React from 'react';
+import React, { useMemo } from 'react';
 import Intro from '../components/Intro';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
-import Social from '../components/Social';
 
-const IndexPage: React.FC<PageProps> = (props) => {
-  const bio = get(props, 'data.allContentfulLandingPageContent.edges[0].node');
+interface BioData {
+  bio: {
+    bio: string;
+  };
+  salutation: string;
+  intro: string;
+}
+
+interface BioQuery {
+  allContentfulLandingPageContent: {
+    edges: Array<{
+      node: BioData;
+    }>;
+  };
+}
+
+interface IndexPageProps extends PageProps {
+  data: BioQuery;
+}
+
+const IndexPage: React.FC<IndexPageProps> = ({ data }) => {
+  const bio = useMemo(() => {
+    return data?.allContentfulLandingPageContent?.edges?.[0]?.node;
+  }, [data]);
+
   return (
     <Layout>
-      <Seo title={'Home'} description={'This is the home page.'} />
+      <Seo
+        title="Home"
+        description="Hemant Nirmalkar - Full Stack Developer. Welcome to my personal portfolio website where I share my projects, blog posts, and professional experience."
+      />
       {bio && <Intro bioData={bio} />}
     </Layout>
   );
